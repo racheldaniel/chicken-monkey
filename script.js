38f6f3597dffd67c25a675fb3d7546096d2bf7f6
Rachel Daniel <rachdaniel88@gmail.com>
2018-10-11 13:30:11 -0500

    fixed math error

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let paragraph = document.getElementsByTagName("p")[0]
 let text = "";
 
 for (let num = 1; num <= 100; num++){
-  if ((num % 5 === 1 ) && (num % 7 == 1)) {
+  if ((num % 5 === 0 ) && (num % 7 == 0)) {
     text += "ChickenMonkey ";
-  } else if (num % 5 == 1 ) {
+  } else if (num % 5 == 0 ) {
     text += "Chicken "
-  } else if (num % 7 == 1 ) {
+  } else if (num % 7 == 0 ) {
     text += "Monkey "
   } else {
     text += `${num} `
